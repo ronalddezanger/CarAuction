13e732070542e7f94ee115356a29b2681e61b5dd
Ronald de Zanger <ronald@ronalddezanger.nl>
2024-01-07 12:23:21 +0100

    end of lecture 85

---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,31 @@
+export default function Home() {
+  return (
+    <div>
+      Content goes here
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+      <p>lala</p>
+    </div>
+  )
+}
